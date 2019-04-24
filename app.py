9de681ef83781d122f657fd5f19aa32b7d0c18bc
phoebejeng <phoebejeng@kkbox.com>
2019-04-24 16:52:52 +0800

    Refactory

--- app.py
+++ app.py
@@ -37,9 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    message = TextSendMessage(text=event.message.text))
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        message
+    )
 
 import os
 if __name__ == "__main__":
