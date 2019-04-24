5a2a2ed5ee73627a61b1af4933b3be15f46b66da
phoebejeng <phoebejeng@kkbox.com>
2019-04-24 17:02:07 +0800

    Fix bug of cannot found os

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, abort
 
 from linebot import (
@@ -43,7 +45,6 @@ def handle_message(event):
         message
     )
 
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
